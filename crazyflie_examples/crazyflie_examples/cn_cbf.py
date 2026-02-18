29722c12473e7fa3d8c56d453a6851c620293859
Bojan Derajic <derajicbojan@gmail.com>
2026-02-18 17:53:02 +0100

    cn-cbf branch created

--- crazyflie_examples/crazyflie_examples/cn_cbf.py
+++ crazyflie_examples/crazyflie_examples/cn_cbf.py
@@ -20,60 +20,6 @@ from torch.nn.utils import vector_to_parameters
 from scipy import ndimage
 from ament_index_python.packages import get_package_share_directory
 
-BASELINE = True
-
-
-def executeTrajectory(
-    timeHelper, cf, trajpath, rate=100, offset=np.zeros(3), yawrate=0.0
-):
-    traj = Trajectory()
-    traj.loadcsv(trajpath)
-
-    start_time = timeHelper.time()
-    yaw = 0
-    while not timeHelper.isShutdown():
-        t = timeHelper.time() - start_time
-        if t > traj.duration:
-            break
-
-        e = traj.eval(t)
-        yaw = yaw + yawrate * 1 / rate
-        omega = e.omega
-        omega[2] = yawrate
-        cf.cmdFullState(
-            e.pos + np.array(cf.initialPosition) + offset, e.vel, e.acc, yaw, omega
-        )
-        print(yaw)
-
-        timeHelper.sleepForRate(rate)
-
-
-# def executeTrajectory2(timeHelper, cf, rate=100, offset=np.zeros(3), yawrate=0.0):
-
-#     start_time = timeHelper.time()
-#     yaw = 0
-#     pos = np.zeros(3)
-#     vel = np.zeros(3)
-#     acc = np.array([0])
-#     while not timeHelper.isShutdown():
-#         t = timeHelper.time() - start_time
-#         if t > 5.0:
-#             break
-
-#         e = traj.eval(t)
-#         yaw = yaw + yawrate * 1/rate
-#         omega = e.omega
-#         omega[2] = yawrate
-#         cf.cmdFullState(
-#             e.pos + np.array(cf.initialPosition) + offset,
-#             e.vel,
-#             e.acc,
-#             yaw,
-#             omega)
-#         print(yaw)
-
-#         timeHelper.sleepForRate(rate)
-
 
 class Sin(nn.Module):
     """Sine activation function"""
@@ -103,47 +49,7 @@ def get_func(func_name: str):
     return func_bank[func_name]
 
 
-class Hypernetwork(nn.Module):
-    """Predicts parameters of the main network based on a 2D input."""
-
-    def __init__(self, input_size: int, output_size: int) -> None:
-        super().__init__()
-        n = 8
-        self.backbone = nn.Sequential(
-            nn.Conv2d(input_size, n, (3, 3), (1, 1), "valid", bias=False),
-            nn.SELU(),
-            nn.Conv2d(n, n, (3, 3), (1, 1), "valid", bias=False),
-            nn.SELU(),
-            nn.AvgPool2d((2, 2), (1, 1)),
-            nn.Conv2d(n, 2 * n, (3, 3), (2, 2), "valid", bias=False),
-            # (2*n, 47, 47)
-            nn.SELU(),
-            nn.Conv2d(2 * n, 2 * n, (3, 3), (1, 1), "valid", bias=False),
-            nn.SELU(),
-            nn.AvgPool2d((2, 2), (1, 1)),
-            nn.Conv2d(2 * n, 4 * n, (2, 2), (2, 2), "valid", bias=False),
-            # (4*n, 22, 22)
-            nn.SELU(),
-            nn.Conv2d(4 * n, 4 * n, (3, 3), (2, 2), "valid", bias=False),
-            nn.SELU(),
-            nn.Conv2d(4 * n, 8 * n, (2, 2), (2, 2), "valid", bias=False),
-            # (8*n, 4, 4)
-            nn.SELU(),
-            nn.Flatten(),
-        )
-
-        self.head = nn.Sequential(
-            nn.Linear(1024, 1024),
-            nn.SELU(),
-            nn.Linear(1024, output_size),
-        )
-
-    def forward(self, input: Tensor) -> Tensor:
-        input = self.backbone(input)
-        return self.head(input)
-
-
-class MainNetwork(nn.Module):
+class MLP(nn.Module):
     """Multi-Layer Perceptron (MLP) model class."""
 
     def __init__(self, config: dict) -> None:
@@ -181,8 +87,8 @@ class DoubleIntegrator2D:
 
         # Control space
         self.nu = 2
-        self.u_min = [-1.0, -1.0]
-        self.u_max = [1.0, 1.0]
+        self.u_min = [-2.0, -2.0]
+        self.u_max = [2.0, 2.0]
         self.B = ca.DM(
             [
                 [0.0, 0.0],
@@ -199,60 +105,59 @@ class DoubleIntegrator2D:
         return self.B
 
 
+def softmin_numpy(x, beta=10.0, axis=None):
+    out = -(1.0 / beta) * np.log(np.sum(np.exp(-beta * x), axis=axis, keepdims=True))
+    return out.squeeze()
+
+
+def softmin_torch(x, beta=10.0, dim=-1):
+    return -torch.logsumexp(-beta * x, dim=dim) / beta
+
+
 class ORN_CBF:
     def __init__(self):
-        self.alpha = 2.0
-        self.sdf_size = 80
-        self.sdf_patch_size = 16
-        self.sdf_res = 0.075
-        self.hypernet_device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.hypernet_weights = "orn_cbf_2d_double_integrator.pth"
-        self.main_net_device = "cpu"
-        self.main_net_config = {
+        self.alpha = 4.0
+        self.beta = 4.0
+        self.r_min = 0.7
+        self.mlp_device = "cpu"
+        self.mlp_weights_filename = "cn_cbf_quadcopter.pth"
+        self.mlp_config = {
             "input_size": 4,
             "layers": [
-                [32, "sin"],
-                [32, "sin"],
-                [32, "sin"],
-                [16, "sin"],
-                [16, "sin"],
-                [16, "sin"],
-                [8, "sin"],
-                [8, "sin"],
-                [8, "sin"],
+                [48, "sin"],
+                [48, "sin"],
+                [24, "sin"],
+                [24, "sin"],
+                [12, "sin"],
+                [12, "sin"],
                 [1, "softplus"],
             ],
         }
 
         self.system_dynamics = DoubleIntegrator2D()
 
-        self.main_net = MainNetwork(config=self.main_net_config).to(
-            self.main_net_device
-        )
-
         pkg_share_dir = get_package_share_directory("crazyflie_examples")
-        hypernet_weights_path = os.path.join(
-            pkg_share_dir, "data", "model_weights", self.hypernet_weights
+        self.mlp = MLP(config=self.mlp_config).to(self.mlp_device)
+        mlp_weights_path = os.path.join(
+            pkg_share_dir, "data", "model_weights", self.mlp_weights_filename
         )
-
-        self.hypernet = Hypernetwork(
-            input_size=1,
-            output_size=self.main_net.num_params(),
-        ).to(self.hypernet_device)
-
-        self.hypernet.load_state_dict(
+        mlp_weights_path = os.path.join(
+            pkg_share_dir, "model_weights", self.mlp_weights_filename
+        )
+        self.mlp.load_state_dict(
             torch.load(
-                f=hypernet_weights_path,
-                map_location=self.hypernet_device,
+                f=mlp_weights_path,
+                map_location=self.mlp_device,
                 weights_only=True,
             )
         )
 
-        # Define symbolic variables used by the CBF-QP
+        # Define CBF-QP and create QP solver
         system_state = ca.MX.sym("system_state", self.system_dynamics.nx)
         nom_control = ca.MX.sym("nom_control", self.system_dynamics.nu)
         control = ca.MX.sym("control", self.system_dynamics.nu)
         cbf_value = ca.MX.sym("cbf_value")
+        cbf_dt = ca.MX.sym("cbf_dt")
         cbf_grad = ca.MX.sym("cbf_grad", self.system_dynamics.nx)
         slack_var = ca.MX.sym("slack_var")
 
@@ -266,7 +171,7 @@ class ORN_CBF:
         # QP constraints
         f = self.system_dynamics.open_loop_dynamics(system_state)
         g = self.system_dynamics.control_jacobian(system_state)
-        cbf_constr = cbf_grad.T @ (f + g @ control) + self.alpha * cbf_value
+        cbf_constr = cbf_grad.T @ (f + g @ control) + cbf_dt + self.alpha * cbf_value
         qp_constr = cbf_constr + slack_var
 
         # QP parameters
@@ -274,36 +179,26 @@ class ORN_CBF:
             system_state,
             nom_control,
             cbf_value,
+            cbf_dt,
             cbf_grad,
         )
 
         # QP options
-        qp_opts = {"error_on_fail": False}
+        qp_opts = {
+            "error_on_fail": False,
+        }
 
         qp = {"x": qp_var, "f": qp_obj, "g": qp_constr, "p": qp_params}
 
         self.qp_solver = ca.qpsol("qp_solver", "qpoases", qp, qp_opts)
 
-        # SDF patch related variables
-        self.patch_idx_min = (self.sdf_size - self.sdf_patch_size) // 2
-        self.patch_idx_max = (self.sdf_size + self.sdf_patch_size) // 2
-        self.sdf_patch_idx = [
-            slice(None),
-            slice(None),
-            slice(self.patch_idx_min, self.patch_idx_max),
-            slice(self.patch_idx_min, self.patch_idx_max),
-        ]
-        self.patch_scale_factor = (self.sdf_patch_size - 1) * self.sdf_res / 2
-
         # Initialize variables
-        self.sdf_patch = torch.ones(
-            (1, 1, self.sdf_patch_size, self.sdf_patch_size),
-            device=self.main_net_device,
-        )
-        vector_to_parameters(
-            torch.zeros(self.main_net.num_params(), device=self.main_net_device),
-            self.main_net.parameters(),
-        )
+        self.people_msg_old = []
+        self.people_msg_time_old = None
+        self.people_states = np.array([])
+        self.people_states_dot = np.array([])
+        self.robot_state = np.zeros(self.system_dynamics.nx)
+        self.nom_control = np.zeros(self.system_dynamics.nu)
 
     def update_main_net(self, costmap_msg: OccupancyGrid):
         # Update the main neural network based on the latest costmap
@@ -334,14 +229,14 @@ class ORN_CBF:
 
             main_net_params = self.hypernet(sdf).detach().to(self.main_net_device)
 
-            vector_to_parameters(main_net_params.squeeze(), self.main_net.parameters())
+            vector_to_parameters(main_net_params.squeeze(), self.mlp.parameters())
             self.sdf_patch = (
                 sdf[self.sdf_patch_idx].clone().detach().to(self.main_net_device)
             )
         else:
             vector_to_parameters(
-                torch.zeros(self.main_net.num_params(), device=self.main_net_device),
-                self.main_net.parameters(),
+                torch.zeros(self.mlp.num_params(), device=self.main_net_device),
+                self.mlp.parameters(),
             )
             self.sdf_patch = torch.ones(
                 (1, 1, self.sdf_patch_size, self.sdf_patch_size),
@@ -357,7 +252,7 @@ class ORN_CBF:
             device=self.main_net_device,
             requires_grad=True,
         )
-        residual_value = self.main_net(system_state[None, ...])
+        residual_value = self.mlp(system_state[None, ...])
         sdf_value = F.grid_sample(
             input=self.sdf_patch.transpose(-1, -2),
             grid=system_state[None, None, None, :2] / self.patch_scale_factor,
